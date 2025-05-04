dbb5a3896af8652abf129f49faf0a5748abd4cb8
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-05-04 13:52:49 -0400

    same as week1 but python

--- week_02_python_intro/problems.py
+++ week_02_python_intro/problems.py
@@ -1,8 +1,33 @@
 def reverse_string(s):
-    pass
+    res = ""
+    for char in s:
+        res = char + res
+    return res
+
+print(reverse_string("boo"))
+
 
 def find_max(lst):
-    pass
+    result_num = lst[0]
+
+    for num in lst:
+        if num > result_num:
+            result_num = num
+    
+    return result_num
+
+print(find_max([0, 2, 4, 2]))
+
 
 def is_palindrome(word):
-    pass
+    temp_str = word 
+    for char in word:
+        curr_char = temp_str[len(temp_str) - 1]
+        temp_str = temp_str[:-1]
+        if char != curr_char:
+            return False
+
+    return True
+
+print(is_palindrome("racecar"))
+print(is_palindrome("treat"))
